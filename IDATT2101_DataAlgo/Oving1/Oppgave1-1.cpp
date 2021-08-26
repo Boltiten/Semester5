d03efaf1379793fcb5f93af79e52fe088e0eb65c
Boltiten <Morten.Stavik.Eggen@gmail.com>
2021-08-26 22:20:54 +0200

    1-1 med enum
    Suuuuper optimal kode, skriver rapport i morgen på skolen.

--- IDATT2101_DataAlgo/Oving1/Oppgave1-1.cpp
+++ IDATT2101_DataAlgo/Oving1/Oppgave1-1.cpp
@@ -1,54 +1,78 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <chrono>
+#include <cstdlib>
+#include <time.h>
+
+enum Handling {
+    INGEN,
+    KJOP,
+    SALG
+};
+
+Handling SISTHANDLING = Handling::INGEN;
 
 int main(){
-    /* int kursforandring[] = {-1, 3,-9,2,2,-1,2,-1,-2,-1, -5, 6};
+    //putte randomme tall fra -10 til 10 i en array, skulle jobbet med vector, men akk ble array nå..
+    srand(time(NULL));
+    signed char kursforandring[10000]; 
     int arrSize = sizeof(kursforandring)/sizeof(kursforandring[0]);
-    std::cout<<"ArrSize: "<< arrSize << std::endl;
+    for (size_t i = 0; i < arrSize; i++)
+    {
+        kursforandring[i] = (rand()%21) -10;
+    }
+
+    //Om du vil bruke den oppgitte tabellen
+    //signed char kursforandring[] = {-1,3,-9,2,2,-1,2,-1,-5};
+    //int arrSize = sizeof(kursforandring)/sizeof(kursforandring[0]);
+    //Printer kjøp på dagene 1,3,6 og salg på 2,5,7
+
+
+    //Ta tid herifra
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     //initialiserer kjøp og salgdag
     int kjopDag = -1;
     int selgDag = -1;
+    int tmp = 0;
+    std::string handlinger;
 
     for (size_t i = 0; i < arrSize; i++)
     {
-        //Setter en kjøp dag
-        if(i<arrSize //ikke siste dag
-        && kursforandring[i]<0 //kursforandringen må være negativ
-        && kursforandring[i+1]>0) //kursen må stige dagen etter
-        {
-            kjopDag = i;
-            std::cout<<"kjøp dag: "<< kjopDag << std::endl;
+        if(kursforandring[i]<0 || kursforandring[i]>0){
+            tmp = kursforandring[i];
         }
 
-        //Setter en salg dag
-        if(kjopDag != -1 //Sjekker at man har kjøpt
-        && kjopDag < i //Sjekker at dagen man kjøpte på er en tidligere dag
-        && kursforandring[i]>0 //Sjekker at kursen er postitiv i dag
-        && kursforandring[i+1]<0) //Sjekker at kursen går ned i morgen
-        {
-            selgDag = i;
-            std::cout<<"selg dag: "<< selgDag << std::endl;
+        //kan kjøpe enten når ingen handling er gjort eller når man har solgt.
+        if(SISTHANDLING != Handling::KJOP){
+            if(i+1<arrSize
+            && tmp<0 
+            && kursforandring[i+1]>0){
+                handlinger += "Kjøp dag: " + std::to_string(i+1) + ".\n";
+                SISTHANDLING = Handling::KJOP;
+            }
         }
-
-        //stygg bug fix for om siste dagen er positiv, så den selger da, siden i+1 er aldri over 0 om i+1 ikke finnes
-        if(i+1 == arrSize 
-        && kursforandring[i]>0){
-            selgDag = i;
-            std::cout<<"selg dag: "<< selgDag << std::endl;
+        //kan selge når sist handling er et kjøp
+        if(SISTHANDLING == Handling::KJOP){
+            if(i+1<arrSize
+            && tmp>0 
+            && kursforandring[i+1]<0){
+                handlinger += "Salg dag: " + std::to_string(i+1) + ".\n";
+                SISTHANDLING = Handling::SALG;
+            } else if (i+1 == arrSize 
+            && tmp>0) {
+                handlinger += "Salg dag: " + std::to_string(i+1) + ".\n";
+                SISTHANDLING = Handling::SALG;
+            }
         }
-    } */
-
-    std::vector<int> kursforandringVector;
-    for (size_t i = 0; i < 10; i++)
-    {
-        int add = (rand()%21) -10;
-            std::cout<<add << std::endl;
-
     }
-    
 
-    
+    //Ta tid til hit
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();    
+    //Skriver ut kjøps og salg dager
+    std::cout<<handlinger<<std::endl;
 
+    //Skriv ut tid
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
     return 0;
 }
