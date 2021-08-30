05d49c42de339992f27a7da26cefe53e87e339ba
Boltiten <Morten.Stavik.Eggen@gmail.com>
2021-08-30 10:52:51 +0200

    Bedre tidtaking

--- IDATT2101_DataAlgo/Oving2/Oppgave2.1-1.cpp
+++ IDATT2101_DataAlgo/Oving2/Oppgave2.1-1.cpp
@@ -11,8 +11,13 @@ int main(){
     //Kjøretid må tas
     double base = 1.001;
     int potens = 5000;
+    double testRecursion;
+    int rounds = 0;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    double testRecursion = recursion(base,potens);
+    do{        
+        testRecursion = recursion(base,potens);
+        rounds++;
+    } while (rounds < 10000);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
     std::cout<<testRecursion<<std::endl;
