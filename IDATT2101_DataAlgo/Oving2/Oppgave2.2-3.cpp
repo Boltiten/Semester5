1fa13220ec2272e52098c26220c7d40973c7699d
Boltiten <Morten.Stavik.Eggen@gmail.com>
2021-08-30 14:52:57 +0200

    DataAlgo riktig timer

--- IDATT2101_DataAlgo/Oving2/Oppgave2.2-3.cpp
+++ IDATT2101_DataAlgo/Oving2/Oppgave2.2-3.cpp
@@ -7,19 +7,20 @@
 double recursion(double x, int n);
 
 int main(){
-
-    //Kjøretid må tas
     double base = 1.001;
     int potens = 5000;
     double testRecursion;
     int rounds = 0;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     do{        
         testRecursion = recursion(base,potens);
         rounds++;
-    } while (rounds < 1000000);
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count()  /pow(10,9)<< "[s]"<< std::endl;
+        end = std::chrono::steady_clock::now();
+    } while (std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() < 1);
+    end = std::chrono::steady_clock::now();
+    std::cout<<"Time spent: ["<<std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count()<<"ms], should be very close to 1s"<<std::endl;
+    std::cout << "Rounds per second: " <<rounds << " rounds in time spent second"<< std::endl;
     std::cout<<testRecursion<<std::endl;
     return 0;
 }
