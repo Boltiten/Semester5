04f68e5e8ec2746f2921887e8fab6b17481c57d7
Boltiten <Morten.Stavik.Eggen@gmail.com>
2021-09-05 16:01:41 +0200

    Data Algo Øving 2 ferdig

--- IDATT2101_DataAlgo/Oving2/Oppgave2.1-pow.cpp
+++ IDATT2101_DataAlgo/Oving2/Oppgave2.1-pow.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
-#include <math.h>
 #include <chrono>
-#include <cstdlib>
-#include <time.h>
-
-double recursion(double x, int n);
+#include <math.h>
 
 int main(){
-    double base = 2;
-    int potens = 10;
-    double testRecursion;
+    double base = 1.0001;
+    int potens = 10000;
+    double result;
     int rounds = 0;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     do{        
-        testRecursion = recursion(base,potens);
-        rounds++;
+        result = pow(base, potens);
         end = std::chrono::steady_clock::now();
+        rounds ++;
     } while (std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() < 1);
     end = std::chrono::steady_clock::now();
     std::cout<<"Time spent: ["<<std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count()<<"ms], should be very close to 1s"<<std::endl;
     std::cout << "Rounds per second: " <<rounds << " rounds in time spent second"<< std::endl;
-    std::cout<<testRecursion<<std::endl;
-    return 0;
-}
-
-double recursion(double x, int n){
-    if(n == 0){
-        return 1;
-    }else if(n%2 == 1){
-        return (x * recursion(x*x, (n-1)/2));
-    }else if(n%2 == 0){
-        return (recursion(x*x, n/2));
-    }
+    std::cout<<result<<std::endl;
     return 0;
 }
