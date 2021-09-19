0b19e26ddb721ee2940a4d7ffecf53b25268f3a0
Boltiten <Morten.Stavik.Eggen@gmail.com>
2021-09-19 23:27:51 +0200

    Algo øving 4 nesten ferdig

--- IDATT2101_DataAlgo/Oving4/Oppgave2.cpp
+++ IDATT2101_DataAlgo/Oving4/Oppgave2.cpp
@@ -78,36 +78,51 @@ int main(){
     //stack
     int stackSize = 0;
     Stack* s;
+
+    //husk å lukke
     if(file.is_open())
     {
+        //les chars fra programmet inn i en vector med char, kunne kanskje lest det rett inn i stacken? men har ikke blitt sånn nå. 
+        //Måtte vite hvor stor stacken skulle være, så må finne alle tegna som skal inn først
+        //Er heller ikke helt vant til c++, og void* var vanskelig å jobbe med, førte til mye strev.
+        //Håper dette er en OK løsning, vet ikke om det var heeelt det som var planen eller ikke.7
         while (file.get(ch)){
             chars.push_back(ch);
         }
+        //finner størrelse på stacken
         for (const auto &ch : chars)
         {
             if(starters(ch) || enders(ch))
             {
                 stackSize++;
-            }
-            
+            }            
         }
+        //oppretter stacken
         s = newStack(stackSize);
-        std::cout<<"Stack Size: "<<stackSize<<std::endl;
-        for (const auto ch : chars)
+        std::cout<<"Stack Size: "<<s->max<<std::endl;
+
+        //putter tegn i stacken
+        for (const auto &ch : chars)
         {
             if(starters(ch)){
                 push(s, (void*)&ch);
-            } else if(enders(ch)){                
+            } else if(enders(ch)){
                 push(s, (void*)&ch);
+                //sjekke om tegnet matcher starteren som ligger øverst i stacken
+                //Bygg en error om det ikke er match.
             }
-
         }
         for (size_t i = 0; i < stackSize; i++)
         {
-            std::cout<<checkStack(s)<<std::endl;
+            //hvis error, print den før stacken
+            //hvis ingen error, print det før stacken
+            std::cout<<((char*)checkStack(s))[0];
             pop(s);
+ 
         }
-        
+        std::cout<<std::endl;
+        std::cout<<"Stack Size: "<<s->antall<<std::endl;
+        //lukk filen
         file.close();
     } else std::cout<<"Couldn't open file.";
     return 0;
